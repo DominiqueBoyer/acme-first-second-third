4624e5dd36d74a30bf47ccf6255febf3262d5c8f
Dominique <dominiqueboyer94@yahoo.com>
2019-08-06 21:11:45 -0700

    stuff

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const firstPeople = document.querySelector('#firstPeople');
+const secondPeople = document.querySelector('#secondPeople');
+const thirdPeople = document.querySelector('#thirdPeople');
+const button2 = document.querySelectorAll('#two');
+
 const slots = ['first', 'second', 'third'];
 
 const users = [
@@ -8,5 +13,71 @@ const users = [
 ];
 
 
+const render = ()=> {
+  const htmlFirst = users.map((person)=>{
+    if(person.slot ==='first'){
+      return `<div> ${person.name}</div>`
+    }
+  }).join('');
+  firstPeople.innerHTML = htmlFirst;
+  const htmlSecond = users.map((person)=>{
+    if(person.slot ==='second'){
+      return `<div> ${person.name}</div>`
+    }
+  }).join('');
+  secondPeople.innerHTML = htmlSecond;
+  const htmlThird = users.map((person)=>{
+    if(person.slot ==='third'){
+      return `<div> ${person.name}</div>`
+    }
+  }).join('');
+  thirdPeople.innerHTML = htmlThird;
+};
 
-console.log('hi')
+render();
+
+firstPeople.addEventListener('click', ev => {
+  console.log(ev.target.innerHTML)
+  if(ev.target.getAttribute('selected')=== 'true'){
+    ev.target.setAttribute('selected', 'false')
+    ev.target.setAttribute('style', 'background-color: white')
+  }
+  else{
+    ev.target.setAttribute('selected', 'true');
+    ev.target.setAttribute('style', 'background-color:tomato');
+    users
+  };
+
+});
+
+secondPeople.addEventListener('click', ev => {
+
+  if(ev.target.getAttribute('selected')=== 'true'){
+    ev.target.setAttribute('selected', 'false')
+    ev.target.setAttribute('style', 'background-color: white')
+  }
+  else{
+    ev.target.setAttribute('selected', 'true');
+    ev.target.setAttribute('style', 'background-color:tomato');
+    users
+  };
+
+});
+
+
+thirdPeople.addEventListener('click', ev => {
+
+  if(ev.target.getAttribute('selected')=== 'true'){
+    ev.target.setAttribute('selected', 'false')
+    ev.target.setAttribute('style', 'background-color: white')
+  }
+  else{
+    ev.target.setAttribute('selected', 'true');
+    ev.target.setAttribute('style', 'background-color:tomato');
+    users
+  };
+
+});
+
+console.log(users)
+
